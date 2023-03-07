ca4a82e72c8c495f43e9761a63864d7b01e718a1
a kaniewski <akaniewski2@gmail.com>
2023-03-07 21:44:25 +0100

    dynamic table w test.html v2

--- src/main/java/pl/arkani/LZ_2022301_LX/Examples/SqlRepoExec.java
+++ src/main/java/pl/arkani/LZ_2022301_LX/Examples/SqlRepoExec.java
@@ -22,8 +22,8 @@ public class SqlRepoExec {
     }
 
 
-    public void SqlRepoExecTest () {
-        List <String> row = new ArrayList<>();
+    public List<List<String>> sqlRepoExecTest (String sql) {
+        List <String> row ;//= new ArrayList<>();
 
         List<List<String>> rows = new ArrayList<List<String>>();
         List <String> rows2 = new ArrayList<>();
@@ -31,55 +31,79 @@ public class SqlRepoExec {
 
 
 
+        //List <String> rows2 = new ArrayList<>();
+
+
+
+
+
 
         List<Object[]> wynik;
-        wynik = sqlRepo.SqlSelectAll("select * from arkani_1.user");
+        wynik = sqlRepo.SqlSelectAll(sql);
 
         wynik.forEach(System.out::println);
 
 
-
+        int aLen;
         for (Object[] a:wynik) {
 
-            row.clear();
+           aLen =  a.length;
+
+
+            row = new ArrayList<>();
            // row.clear();
             for (int i = 0; i <a.length ; i++) {
+
+
+
+
+
+                System.out.println( "----------------------------");
                 System.out.println(a[i]);
+                System.out.println("MOD:" +(i % a.length-1));
+                System.out.println( "----------------------------");
                 row.add(String.valueOf(a[i]));
-                rows.add(row);
+
+
+
+
             }
+            System.out.println( "----------------------------");
+            rows.add(row);
+
 
 
         }
 
 
-
-
-        row.forEach(System.out::println);
+        System.out.println( "--------------------");
+       // row.forEach(System.out::println);
+        System.out.println( "---------------");
         rows.forEach(System.out::println);
 
 
-
-
+        return rows;
     }
 
-    @Component
-    class SqlRepoExecTest {
-
-
-        private SqlRepoExec sqlRepoExec;
-
-        SqlRepoExecTest(SqlRepoExec sqlRepoExec) {
-            this.sqlRepoExec = sqlRepoExec;
-        }
-
-        @EventListener(ApplicationReadyEvent.class)
-        public void runExample() throws UnknownHostException {
-
-            System.out.println("# sqlRepoExec.SqlRepoExecTest()");
-            sqlRepoExec.SqlRepoExecTest();
-        }
-    }
+//    @Component
+//    class SqlRepoExecTest {
+//
+//
+//        private SqlRepoExec sqlRepoExec;
+//
+//        SqlRepoExecTest(SqlRepoExec sqlRepoExec) {
+//            this.sqlRepoExec = sqlRepoExec;
+//        }
+//
+//        @EventListener(ApplicationReadyEvent.class)
+//        public void runExample() throws UnknownHostException {
+//
+//            System.out.println("# sqlRepoExec.SqlRepoExecTest()");
+//            sqlRepoExec.sqlRepoExecTest("select * from arkani_1.user");
+//
+//
+//        }
+//    }
 
 
 
--- src/main/java/pl/arkani/LZ_2022301_LX/controller/TestController.java
+++ src/main/java/pl/arkani/LZ_2022301_LX/controller/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.arkani.LZ_2022301_LX.Examples.SqlRepo;
+import pl.arkani.LZ_2022301_LX.Examples.SqlRepoExec;
 import pl.arkani.LZ_2022301_LX.model.Test;
 import pl.arkani.LZ_2022301_LX.repo.TestRepo;
 
@@ -23,9 +25,11 @@ public class TestController {
 
 
     private TestRepo testRepo;
+    private SqlRepoExec sqlRepoExec;
     @Autowired
-    public TestController(TestRepo testRepo) {
+    public TestController(TestRepo testRepo, SqlRepoExec sqlRepoExec) {
         this.testRepo = testRepo;
+        this.sqlRepoExec = sqlRepoExec;
     }
 
     @GetMapping("test/add")
@@ -50,18 +54,32 @@ public class TestController {
     public String showTestList(Model model) {
 
         List<String> headers = Arrays.asList("ID", "Name", "Salary", "Status");
-        List<Map<String, Object>> rows = new ArrayList<>();
-        rows.add(Map.of("ID", "1", "Name", "Jim", "Salary", "50000", "Status", "active"));
-        rows.add(Map.of("ID", "2", "Name", "Sally", "Salary", "50000", "Status", "inactive"));
+        List<Map<String, Object>> rows2 = new ArrayList<>();
+        rows2.add(Map.of("ID", "1", "Name", "Jim", "Salary", "50000", "Status", "active"));
+        rows2.add(Map.of("ID", "2", "Name", "Sally", "Salary", "50000", "Status", "inactive"));
 
         for (Test  t: testRepo.findAll()) {
 
-            rows.add(Map.of(headers.get(0),t.getId(),headers.get(1),t.getName(),headers.get(2),"50",headers.get(3),"active"));
+            rows2.add(Map.of(headers.get(0),t.getId(),headers.get(1),t.getName(),headers.get(2),"50",headers.get(3),"active"));
          //   rows.add(Map.of(headers.get(0),t.getId()+10));
-            rows.add(Map.of(headers.get(1),t.getName()));
-            rows.add(Map.of(headers.get(3),"no active"));
+            rows2.add(Map.of(headers.get(1),t.getName()));
+            rows2.add(Map.of(headers.get(3),"no active"));
 
         }
+
+
+        /*List<String> */headers = Arrays.asList("id", "username", "role");
+        List<List<String>> rows= sqlRepoExec.sqlRepoExecTest("select id,username,role from arkani_1.user order by id");
+
+
+        model.addAttribute("test", testRepo.findAll());
+        model.addAttribute("headers",headers);
+
+        model.addAttribute("headers",headers);
+        model.addAttribute("rows2", rows2);
+        model.addAttribute("rows", rows);
+        return "test/test";
+
 //
 //        List<Test> listOfClass = new ArrayList<>();
 //
@@ -81,10 +99,7 @@ public class TestController {
 
 
 
-        model.addAttribute("test", testRepo.findAll());
-        model.addAttribute("headers",headers);
-        model.addAttribute("rows", rows);
-        return "test/test";
+
     }
 
     @GetMapping("test/edit/{id}")
